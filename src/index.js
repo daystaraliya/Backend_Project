5d525a17647e2b5ce7006ddb5573c65355f92053
daystaraliya <daystaraliya>
2025-02-22 06:13:19 +0530

    Database connected successfully

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import 'dotenv/config'
+import connectDB from './db/index.js'
+
+connectDB()
+
+
+
+
+
+
+
+
+// import express from "express"
+// const app = express()
+
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.DBURL}/${DB_NAME}`)
+//         app.on("error" , (error) => {
+//             console.log("ERROR" , error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT,() => {
+//             console.log(`App is listening ${
+//                 process.env.PORT}`);
+//         })
+//     }
+//     catch(error) {
+//         console.error("ERROR" , error)
+//     }
+// })()
