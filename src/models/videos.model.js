9062ba678584a464a282f6bf60126644cf9abd9e
daystaraliya <daystaraliya>
2025-02-22 22:14:37 +0530

    added user and videos model

--- src/models/videos.model.js
+++ src/models/videos.model.js
@@ -1,38 +1,44 @@
-import mongoose from "mongoose"
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videosSchema = new mongoose.Schema({
-    videoFile:{
-        type:String,
-        required:true
+const videosSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
     },
-    thumbnail:{
-        type : String
-        
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //cloudinay url
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
-    },
-    owner:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref : "User"
-    },
-    title:{
-        type:String
-    },
-    description:{
-        type:String
-    },
-    duration:{
-        type:Number,
-        required:true
-    },
-    views:{
-        type:Number,
-        required:true
-    },
-    isPublished:{
-        type:Boolean
-    },
-    
+//videosSchema.plugin(mongooseAggregatePaginate)
 
-},{timestamps:true})
-
-export const Video = mongoose.model("Video" , videosSchema)
+export const Video = mongoose.model("Video", videosSchema);
