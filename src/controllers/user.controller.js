63d250322da2d2c406e4ea16354b793bca65b1f2
daystaraliya <daystaraliya>
2025-02-24 10:52:16 +0530

    typo bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -262,7 +262,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "User fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -270,7 +270,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
   if (!(fullName || email)) {
     throw new ApiError(400, "all fields are required");
   }
-  User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
